108398a32964a618987a00d69c42edb6e1554a83
dibkb <dibas9110@gmail.com>
2024-08-05 01:08:40 +0530

    migrating to express

--- ws-2/server/index.js
+++ ws-2/server/index.js
@@ -1,8 +1,11 @@
-import { createServer } from "http";
+import express from "express";
 import { Server } from "socket.io";
-
-const httpServer = createServer();
-const io = new Server(httpServer, {
+const PORT = 3000;
+const app = express();
+const expressServer = app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+const io = new Server(expressServer, {
   cors: {
     origin:
       process.env.NODE_ENV === "production"
@@ -17,8 +20,4 @@ io.on("connection", (socket) => {
     console.log(data);
     io.emit("message", data);
   });
-});
-
-httpServer.listen(3000, () => {
-  console.log("server running on http://localhost:3000");
 });
